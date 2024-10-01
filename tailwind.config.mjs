e7a84d67b2f836946c9bf26cf2b27a9c941d9014
César Jarith Coello <jarith182@gmail.com>
2024-10-01 15:57:18 -0600

    componente enlace

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class', // Habilita el soporte para el modo oscuro
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {

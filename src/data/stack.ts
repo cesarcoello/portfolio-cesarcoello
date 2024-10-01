388c4d30d83512b90013bbd5f9afa01b3c9bd59a
César Jarith Coello <jarith182@gmail.com>
2024-10-01 17:51:26 -0600

    stack componetizado

---
+++ src/data/stack.ts
@@ -0,0 +1,16 @@
+export const stack = [
+    {name:'Html', icon:"/stack/html.svg" },
+    {name:'Css', icon:"/stack/css.svg" },
+    {name:'Javascript', icon:"/stack/javascript.svg" },
+    {name:'Bootstrap', icon:"/stack/bootstrap.svg" },
+    {name:'Tailwind', icon:"/stack/tailwind.svg" },
+    {name:'Astro', icon:"/stack/astro.svg" },
+    {name:'React', icon:"/stack/react.svg" },
+    {name:'MySql', icon:"/stack/mysql.svg" },
+    {name:'SQLServer', icon:"/stack/sqlserver.svg" },
+    {name:'PLSQL', icon:"/stack/plsql.svg" },
+    {name:'SQLDeveloper', icon:"/stack/sqldeveloper.svg" },
+    {name:'Figma', icon:"/stack/figma.svg" },
+    {name:'Git', icon:"/stack/git.svg" },
+    {name:'Git Hub', icon:"/stack/github.svg" },
+]
